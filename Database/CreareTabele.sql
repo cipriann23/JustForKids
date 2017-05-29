71e37f01b774f1e6d45f581edea1bf73c360bb56
cipriann23 <nemtocciprian10@gmail.com>
2017-05-29 15:26:37 +0300

    Populare treste

--- Database/CreareTabele.sql
+++ Database/CreareTabele.sql
@@ -52,17 +52,31 @@ data TIMESTAMP
 Create Table TW_checkpoint(
 id_kid integer,
 id_test integer,
-id_intrebare integer
+id_intrebare integer,
+category integer
 )
 /
 INSERT into tw_login values(0,'admin',null,null,'admin',2,'nemtocciprian10@gmail.com',null,null);
-INSERT into tw_login values(1,'kid','Popescu','Andrei','kid',0,null,null,null);
+INSERT into tw_login values(3,'Andrei','Popescu','Andrei','popescu',0,null,null,null);
+INSERT into tw_login values(4,'Costel','Popescu','Costel','popescu',0,null,null,null);
 INSERT into tw_login values(2,'parent','Popescu','Marghioala','parent',1,'nem.ciprian@yahoo.com','Iasi','Romania');
 /
-INSERT into TW_CHECKPOINT values(1,1,5);
-INSERT into TW_kid values(1,2,7);
+INSERT into TW_CHECKPOINT values(3,1,1,0);
+INSERT into TW_CHECKPOINT values(3,1,1,1);
+INSERT into TW_CHECKPOINT values(3,1,1,2);
+
+INSERT into TW_CHECKPOINT values(4,1,1,0);
+INSERT into TW_CHECKPOINT values(4,1,1,1);
+INSERT into TW_CHECKPOINT values(4,1,1,2);
+
+
+INSERT into TW_kid values(3,2,7);
+INSERT into TW_kid values(4,2,14);
+
 UPDATE TW_checkpoint SET id_test=1, id_intrebare=1 where id_kid=1;
 /
 delete from tw_answers;
 /
 commit;
+/
+drop table Tw_checkpoint;
--- Database/Insert_tests.sql
+++ Database/Insert_tests.sql
@@ -1,5 +1,6 @@
+--GEOGRAFIE
 INSERT into tw_test values(1,1,0,1,'Care dintre urmatoarele reprezentari de coordonate este cea corecta?','1_11_coordonate.jpg','1_12_coordonate.jpg','1_13_coordonate.jpg','1_14_coordonate.jpg',3);
-INSERT into tw_test values(1,1,0,2,'Pe harta afisata am marcat mai multe tari din EUROPA. Alegeti harta pe care este marcata ROMANIA.','1_21_romania.jpg','1_22_romania.jpg','1_23_romania.jpg','1_24_romania.jpg',2);
+INSERT into tw_test values(1,1,0,2,'Pe harta afisata am marcat mai multe tari din EUROPA. Alegeti harta pe care este marcata ROMANIA.','1_21_romania.jpg','1_22_romania.jpg','1_23_romania.jpg','1_24_romania.jpg',4);
 INSERT into tw_test values(1,1,0,3,'Alegeti harta pe care este marcat judetul CONSTANTA in mod corect.','1_31_constanta.jpg','1_32_constanta.jpg','1_33_constanta.jpg','1_34_constanta.jpg',1);
 INSERT into tw_test values(1,1,0,4,'Alegeti capitala corespunzatoare statului Roman.','1_41_capitala.jpg','1_42_capitala.jpg','1_43_capitala.jpg','1_44_capitala.jpg',1);
 INSERT into tw_test values(1,1,0,5,'Pe ce continent se afla tara ROMANIA?','1_51_continent.jpg','1_52_continent.jpg','1_53_continent.jpg','1_54_continent.jpg',4);
@@ -8,3 +9,58 @@ INSERT into tw_test values(1,1,0,7,'Care dintre urmatoarele regiuni este marcata
 INSERT into tw_test values(1,1,0,8,'Alegeti reprezentarea corecta pentru Polul Nord, Polul Sud si Ecuator.','1_81_globe.jpg','1_82_globe.jpg','1_83_globe.jpg','1_84_globe.jpg',1);
 INSERT into tw_test values(1,1,0,9,'Pentru reprezentarile urmatoare alegeti harta in care este reprezentat corect vecinul romaniei.','1_91_vecin.jpg','1_92_vecin.jpg','1_93_vecin.jpg','1_94_vecin.jpg',3);
 INSERT into tw_test values(1,1,0,10,'Romania are granita maritima marea ...','1_101_sea.jpg','1_102_sea.jpg','1_103_sea.jpg','1_104_sea.jpg',1);
+/
+INSERT into tw_test values(1,1,1,1,'Alegeti vecinii corecti din partea de nord a Italiei:','1_11_italia.jpg','1_12_italia.jpg','1_13_italia.jpg','1_14_italia.jpg',4);
+INSERT into tw_test values(1,1,1,2,'Care dintre urmatorii munti aprtine lantului muntos Fagarasi si este cel mai inalt munte din Romania, avand o inaltime de 2544 m?','1_21_fagarasi.jpg','1_22_fagarasi.jpg','1_23_fagarasi.jpg','1_24_fagarasi.jpg',1);
+INSERT into tw_test values(1,1,1,3,'Care este singurul lac vulcanic din Romania?','1_31_vulcanic.jpg','1_32_vulcanic.jpg','1_33_vulcanic.jpg','1_34_vulcanic.jpg',3);
+INSERT into tw_test values(1,1,1,4,'Unde isi are locul de varsare Raul Prut?','1_41_dunare.jpg','1_42_dunare.jpg','1_43_dunare.jpg','1_44_dunare.jpg',2);
+INSERT into tw_test values(1,1,1,5,'Dunarea al doilea fluviu din Europa, izvoraste de pe urmatorul teritoriu:','1_51_germania.jpg','1_52_germania.jpg','1_53_germania.jpg','1_54_germania.jpg',4);
+INSERT into tw_test values(1,1,1,6,'Care din urmatoarele capitale este capitala Frantei?','1_61_franta.jpg','1_62_franta.jpg','1_63_franta.jpg','1_64_franta.jpg',1);
+INSERT into tw_test values(1,1,1,7,'Care dintre urmatoarele turnuri este reprezentat de turnul din Pisa?','1_71_pisa.jpg','1_72_pisa.jpg','1_73_pisa.jpg','1_74_pisa.jpg',1);
+INSERT into tw_test values(1,1,1,8,'Care dintre urmatoarele drumuri este reprezentat de Transfagarasan?','1_81_drum.jpg','1_82_drum.jpg','1_83_drum.jpg','1_84_drum.jpg',2);
+INSERT into tw_test values(1,1,1,9,'Care dintre urmatorii munti nu contin roci vulcanice?','1_91_vulcanic.jpg','1_92_vulcanic.jpg','1_93_vulcanic.jpg','1_94_vulcanic.jpg',4);
+INSERT into tw_test values(1,1,1,10,'Care tara din lume are cea mai mare suprafata?','1_101_rusia.jpg','1_102_rusia.jpg','1_103_rusia.jpg','1_104_rusia.jpg',3);
+--MATEMATICA
+INSERT into tw_test values(1,0,0,1,'Alegeti varianta corecta:','1_11_adunare.jpg','1_12_adunare.jpg','1_13_adunare.jpg','1_14_adunare.jpg',4);
+INSERT into tw_test values(1,0,0,2,'Pe ce linie se gasesc cele mai putine albine?','1_21_albina.jpg','1_22_albina.jpg','1_23_albina.jpg','1_24_albina.jpg',3);
+INSERT into tw_test values(1,0,0,3,'Care dintre urmatoarele scaderi sunt eronate?','1_31_trei.jpg','1_32_trei.jpg','1_33_trei.jpg','1_34_trei.jpg',2);
+INSERT into tw_test values(1,0,0,4,'Cate dintre pasari trebuie sa plece pentru a ramane doar cea rosie si galbena?','1_41_pasari.jpg','1_42_pasari.jpg','1_43_pasari.jpg','1_44_pasari.jpg',1);
+INSERT into tw_test values(1,0,0,5,'De cate sageti ar fi nevoie pentru a ramane doar 5 baloane?','1_51_sageti.jpg','1_52_sageti.jpg','1_53_sageti.jpg','1_54_sageti.jpg',1);
+INSERT into tw_test values(1,0,0,6,'Pentru ca toti cei 21 de colegi ai tai sa primeasca cate o ciocolata, cate ciocolate mai trebuie sa cumperi?','1_61_ciocolata.jpg','1_62_ciocolata.jpg','1_63_ciocolata.jpg','1_64_ciocolata.jpg',2);
+INSERT into tw_test values(1,0,0,7,'Daca pauza ar fi la 10:30, cate minute trebuie sa mai treaca pana va suna clopotelul?','1_71_ceas.jpg','1_72_ceas.jpg','1_73_ceas.jpg','1_74_ceas.jpg',1);
+INSERT into tw_test values(1,0,0,8,'Stiind ca mai ai 8 frati, iar o cutie contine 3 inghetate, cate cutii trebuie sa cumperi sa poti manca o inghetata impreuna cu fratii tai?','1_81_cutie.jpg','1_82_cutie.jpg','1_83_cutie.jpg','1_84_cutie.jpg',3);
+INSERT into tw_test values(1,0,0,9,'Pentru o familie de doi porumbei, de cati porumbei ar mai fi nevoie pentru a forma 4 familii?','1_91_familie.jpg','1_92_familie.jpg','1_93_familie.jpg','1_94_familie.jpg',2);
+INSERT into tw_test values(1,0,0,10,'De cate culori ai nevoie pentru ca si celelalte floricele sa fie colorate la fel?','1_101_culori.jpg','1_102_culori.jpg','1_103_culori.jpg','1_104_culori.jpg',1);
+/
+INSERT into tw_test values(1,0,1,1,'Rezolvati corect acest exercitiu!','1_11_calcule.jpg','1_12_calcule.jpg','1_13_calcule.jpg','1_14_calcule.jpg',1);
+INSERT into tw_test values(1,0,1,2,'Care din urmatorii elevi nu a invatat deloc inmultirea cu doi?','1_21_inmultire.jpg','1_22_inmultire.jpg','1_23_inmultire.jpg','1_24_inmultire.jpg',2);
+INSERT into tw_test values(1,0,1,3,'Daca un set de mingi este format din aceste mingi, cate seturi mai nevoie pentru cei 36 de colegi stiind ca un set l-ai cumparat deja?','1_31_minge.jpg','1_32_minge.jpg','1_33_minge.jpg','1_34_minge.jpg',3);
+INSERT into tw_test values(1,0,1,4,'Stiind ca o pizza intreaga cuprinde 5 felii, de cate pizza vor fi nevoie pentru 40 de copii?','1_41_pizza.jpg','1_42_pizza.jpg','1_43_pizza.jpg','1_44_pizza.jpg',4);
+INSERT into tw_test values(1,0,1,5,'Cate cosulete mai ai nevoie pentru a oferi la fiecare invatatoare cate unul stiind ca in total sunt 14 invatatoare dintre care 2 lipsesc iar tu ai cumparat deja 12?','1_51_ghiocei.jpg','1_52_ghiocei.jpg','1_53_ghiocei.jpg','1_54_ghiocei.jpg',4);
+INSERT into tw_test values(1,0,1,6,'Daca stii cand este 7x5 alege numarul corespunzator!','1_61_sase.jpg','1_62_sase.jpg','1_63_sase.jpg','1_64_sase.jpg',3);
+INSERT into tw_test values(1,0,1,7,'Care este jumatatea lui 24?','1_71_jumate.jpg','1_72_jumate.jpg','1_73_jumate.jpg','1_74_jumate.jpg',1);
+INSERT into tw_test values(1,0,1,8,'Cate masini au mai ramas pe pista stiind ca numarul lor a scazut de sase ori iar la inceput erau 48?','1_81_masini.jpg','1_82_masini.jpg','1_83_masini.jpg','1_84_masini.jpg',1);
+INSERT into tw_test values(1,0,1,9,'Daca una din masini nu functioneaza, cate echipe se pot forma din restul masinilor avand in vedere ca o echipa trebuie sa contina 2 masini?','1_91_echipe.jpg','1_92_echipe.jpg','1_93_echipe.jpg','1_94_echipe.jpg',4);
+INSERT into tw_test values(1,0,1,10,'Initial in acest cos au fost 24 de mere. Cate au mai ramas daca: 6 ai dat la colegi, apoi, din ce a ramas jumatate le-ai dat fratilor tai acasa, 3 le-ai mancat tu, apoi parintii ti-au mai daruit inca jumatate din cat aveai la inceput!','1_101_mere.jpg','1_102_mere.jpg','1_103_mere.jpg','1_104_mere.jpg',1);
+--BIOLOGIE
+INSERT into tw_test values(1,2,0,1,'Care dintre urmatoarele vietuitoare traiesc in mediu acvatic?','1_11_pesti.jpg','1_12_pesti.jpg','1_13_pesti.jpg','1_14_pesti.jpg',1);
+INSERT into tw_test values(1,2,0,2,'Care dintre urmatoarele flori este ocrotita de lege in Romania?','1_21_lege.jpg','1_22_lege.jpg','1_23_lege.jpg','1_24_lege.jpg',3);
+INSERT into tw_test values(1,2,0,3,'Care dintre urmatoarele vietuitoare nu sunt pe cale de disparitie in Romania?','1_31_disparitie.jpg','1_32_disparitie.jpg','1_33_disparitie.jpg','1_34_disparitie.jpg',1);
+INSERT into tw_test values(1,2,0,4,'Care este cea mai mica pasare din lume, avand o dimensiune de aproximativ 22 cm?','1_41_colibri.jpg','1_42_colibri.jpg','1_43_colibri.jpg','1_44_colibri.jpg',2);
+INSERT into tw_test values(1,2,0,5,'Care dintre urmatoarele linii reprezinta pistilul unei plante?','1_51_pistil.jpg','1_52__pistil.jpg','1_53__pistil.jpg','1_54_pistil.jpg',3);
+INSERT into tw_test values(1,2,0,6,'Care dintre urmatoarele fotografii corespunde regnului fungilor?','1_61_fungi.jpg','1_62_fungi.jpg','1_63_fungi.jpg','1_64_fungi.jpg',1);
+INSERT into tw_test values(1,2,0,7,'Alege semnul corect prin care poate fi identificat un baiat:','1_71_baiat.jpg','1_72_baiat.jpg','1_73_baiat.jpg','1_74_baiat.jpg',1);
+INSERT into tw_test values(1,2,0,8,'In ce poza poti identifica membrele superioare?','1_81_superior.jpg','1_82_superior.jpg','1_83_superior.jpg','1_84_superior.jpg',2);
+INSERT into tw_test values(1,2,0,9,'Pentru a putea vizualiza detaliile undei celule avem nevoie de:','1_91_microscop.jpg','1_92_microscop.jpg','1_93_microscop.jpg','1_94_microscop.jpg',2);
+INSERT into tw_test values(1,2,0,10,'In care din urmatoarele imagini putem identifica nucleul celular?','1_101_nucleu.jpg','1_102_nucleu.jpg','1_103_nucleu.jpg','1_104_nucleu.jpg',4);
+/
+INSERT into tw_test values(1,2,1,1,'Care dintre urmatoarele degete este reprezentat de index?','1_11_mana.jpg','1_12_mana.jpg','1_13_mana.jpg','1_14_mana.jpg',1);
+INSERT into tw_test values(1,2,1,2,'Care dintre urmatoarele organe este inima?','1_21_inima.jpg','1_22_inima.jpg','1_23_inima.jpg','1_24_inima.jpg',3);
+INSERT into tw_test values(1,2,1,3,'Care este cel mai lung os din corpul omenesc?','1_31_os.jpg','1_32_os.jpg','1_33_os.jpg','1_34_os.jpg',4);
+INSERT into tw_test values(1,2,1,4,'In care poza puteam gasi osul frontal?','1_41_frontal.jpg','1_42_frontal.jpg','1_43_frontal.jpg','1_44_frontal.jpg',2);
+INSERT into tw_test values(1,2,1,5,'Cate cavitati are inima?','1_51_cavitati.jpg','1_52_cavitati.jpg','1_53_cavitati.jpg','1_54_cavitati.jpg',1);
+INSERT into tw_test values(1,2,1,6,'Care este cea mai importanta functie a plamandului?','1_61_plaman.jpg','1_62_plaman.jpg','1_63_plaman.jpg','1_64_plaman.jpg',3);
+INSERT into tw_test values(1,2,1,7,'In care poza se poate observa muschiul sternocleidomastoidian?','1_71_muschi.jpg','1_72_muschi.jpg','1_73_muschi.jpg','1_74_muschi.jpg',1);
+INSERT into tw_test values(1,2,1,8,'Cati dinti are in total un adult?','1_81_dinti.jpg','1_82_dinti.jpg','1_83_dinti.jpg','1_84_dinti.jpg',1);
+INSERT into tw_test values(1,2,1,9,'Alegeti aspectul cel mai corect al acestei celule:','1_91_neuron.jpg','1_92_neuron.jpg','1_93_neuron.jpg','1_94_neuron.jpg',1);
+INSERT into tw_test values(1,2,1,10,'Care dintre urmatoarele organite celulare corespunde mitocondriei, responsabila pentru producerea de energie in corpul uman:','1_101_mitocondria.jpg','1_102_mitocondria.jpg','1_103_mitocondria.jpg','1_104_mitocondria.jpg',3);
